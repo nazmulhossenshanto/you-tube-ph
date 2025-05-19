13efd0211c461fb8411a14483e0f8968689d7232
shanto <mdnazmulhossenshanto01@gmail.com>
2025-05-20 04:32:41 +0600

    category btn added

--- youtube.js
+++ youtube.js
@@ -1,1 +1,21 @@
-console.log('youtube file added')
+// load categories and display category buttons
+const loadCategories = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phero-tube/categories"
+  );
+  const data = await res.json();
+  displayCategories(data.categories);
+};
+loadCategories();
+const displayCategories = (categories) => {
+  const categoryBtnContainer = document.getElementById(
+    "category-btn-container"
+  );
+  categories.forEach((category) => {
+    //console.log(category);
+    const btn = document.createElement("button");
+    btn.classList = 'btn'
+    btn.innerText = category.category;
+    categoryBtnContainer.appendChild(btn);
+});
+};
